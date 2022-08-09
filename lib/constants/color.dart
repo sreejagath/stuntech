958ae2f4f32e3c56e89dbe99405985680ae1dd69
Jagal R Nath <jagalsree007@gmail.com>
2022-08-09 22:09:46 +0530

    feat: UI implementation

---
+++ lib/constants/color.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+var mainText = const Color(0xFFFAAA14);
+const black = Color(0xFF000000);
--- lib/view/job_details.dart
+++ lib/view/job_details.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:stuntech/constants/color.dart';
 
 class JobDetails extends StatelessWidget {
   const JobDetails({Key? key}) : super(key: key);
@@ -39,10 +41,284 @@ class JobDetails extends StatelessWidget {
                   ),
                 ),
               ),
-            ])
+            ]),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Text(
+                    '[Sapporo, Hokkaido] Nursing care business in a short stay (mainly bathing assistance), please!',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: GoogleFonts.notoSans().fontFamily,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        '6,000 yen',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: GoogleFonts.notoSans().fontFamily,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        color: const Color(0xFFEEAB40).withOpacity(0.1),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          'Paid nursing home for elderly',
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: mainText,
+                            fontFamily: GoogleFonts.notoSans().fontFamily,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    'May 31st (Wednesday) 08: 00 ~ 17: 00 (break 60 minutes)',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: black,
+                                      fontFamily:
+                                          GoogleFonts.notoSans().fontFamily,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    '3-916-17 Shinonome-cho, Sapporo-shi, Hokkaido',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: black,
+                                      fontFamily:
+                                          GoogleFonts.notoSans().fontFamily,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    'Transp. Costs ¥ 300',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: black,
+                                      fontFamily:
+                                          GoogleFonts.notoSans().fontFamily,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.2,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(Radius.circular(5)),
+                          border: Border.all(
+                            color: const Color(0xFFFAAA14),
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                          children: [
+                            Expanded(
+                                child: Text(
+                              'Recruitment number 3 people',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: const Color(0xFFFAAA14),
+                                fontFamily: GoogleFonts.notoSans().fontFamily,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Residential pay nursing home Himawari Club',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: const Color(0xFF303030).withOpacity(0.45),
+                          fontFamily: GoogleFonts.notoSans().fontFamily,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Row(
+                    children: const [
+                      Text(
+                        '😄',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '999',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Row(
+                    children: const [
+                      Text(
+                        '🙂',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '999+',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Row(
+                    children: const [
+                      Text(
+                        '😔',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '999',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  const Icon(Icons.arrow_forward_ios, color: Colors.grey)
+                ],
+              ),
+            ),
           ],
         ),
       ),
+      bottomSheet: BottomAppBar(
+          child: Container(
+        height: 60,
+        child: Row(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 0.2,
+              child: const Icon(
+                Icons.favorite_border,
+                color: Color.fromARGB(255, 121, 121, 121),
+                size: 35,
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: 40,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Color(0xFFFAAA14),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                'Apply',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                  fontFamily: GoogleFonts.notoSans().fontFamily,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
